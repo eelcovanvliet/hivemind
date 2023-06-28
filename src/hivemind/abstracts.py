546f1ab9441d4aef7ad06e8d5ec7755ac2285705
eelco van Vliet <eelcovanvliet@gmail.com>
2023-06-28 13:19:31 +0200

    refactor and doc

--- src/hivemind/abstracts.py
+++ src/hivemind/abstracts.py
@@ -5,7 +5,44 @@ from carg_io.abstracts import ParameterSet
 
 
 class Base(ABC):
+    """Base offers a template for create parameteric object supporting finite states.
+    
+    E.g. A class BallastTank(Base) will accept a ParameterSet outlining the static properties
+    of the tank (such as width, height, length) and may have a finite number of different
+    states, such as Empty, InterMediate, Full.
 
+    see https://refactoring.guru/design-patterns/template-method
+    
+    -------------------------------------------------------------------------------
+
+    JUSTIFICATION of states:
+
+    In favor of state:
+
+    What is close to reality? What is the Structure?
+    Does it include ballast?
+    Does it include the assemmbly of new parts?
+
+    If a structure has been laying in the same spot for 20 years,
+    it is the same structure, but the properties changed.
+    i.e. it now has marine growth, or it was partly corroded.
+    Do we create a NEW instance for that? Does not seem not close to reality.
+    
+    However, say you have a floater, and you stick a turbine on top.
+    Now it's a pretty different thing.
+
+    The parameteric part will be handled by a builder pattern:    
+    https://refactoring.guru/design-patterns/builder
+
+    The potential for an object to switch states will be handled by a state-pattern:
+    https://refactoring.guru/design-patterns/state
+
+    So now, the structure will return different values for the same method,
+    depending on what state is is.
+    
+
+    
+    """
     @abstractproperty
     def parameters(self) -> ParameterSet:
         ...
@@ -13,33 +50,9 @@ class Base(ABC):
     @abstractproperty
     def state(self) -> State:
         """Should return the currently active state.
-
-        -------------------------------------------------------------------------------
-
-        JUSTIFICATION:
-
-        In favor of state:
-
-        What is close to reality? What is the Structure?
-        Does it include ballast?
-        Does it include the assemmbly of new parts?
-
-        If a structure has been laying in the same spot for 20 years,
-        it is the same structure, but the properties changed.
-        i.e. it now has marine growth, or it was partly corroded.
-        Do we create a NEW instance for that? Does not seem not close to reality.
         
-        However, say you have a floater, and you stick a turbine on top.
-        Now it's a pretty different thing.
 
-        The parameteric part will be handled by a builder pattern:    
-        https://refactoring.guru/design-patterns/builder
 
-        The potential for an object to switch states will be handled by a state-pattern:
-        https://refactoring.guru/design-patterns/state
-
-        So now, the structure will return different values for the same method,
-        depending on what state is is.
 
         """
         ...
