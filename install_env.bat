085a3deaed55fccd48eb5eb17ea619c1e299ebfd
eelco van Vliet <eelcovanvliet@gmail.com>
2023-09-18 11:56:59 +0200

    move carg-io from install_env to setup

--- install_env.bat
+++ install_env.bat
@@ -1,20 +1,9 @@
-
-REM create requirements.txt 
-REM NOTE: on windows % is escaped using %%
-REM =======================================
-echo # requirements > requirements.txt
-echo carg-io @ git+https://github.com/eelcovanvliet/carg-io.git >> requirements.txt
-
 REM inplace environment creation
 REM =======================================
 call conda remove --prefix ./.venv --all -y
 call conda create --prefix ./.venv -y
 call activate ./.venv
 call conda install pip -y
-call pip install -r requirements.txt
-
-REM git init
-REM echo *.txt > .gitignore
-REM git submodule add https://github.com/eelcovanvliet/carg-io.git
+call pip install -e .[testing]
 
 cmd /k
